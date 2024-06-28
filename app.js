0a8d354a2d49eb67b02fd365ce23bf64ee2f8081
LaraPsc123 <larapascarella@gmail.com>
2024-06-28 17:24:05 +0200

    ADD: script java

---
+++ app.js
@@ -0,0 +1,27 @@
+//1 Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
+let number = 100 // numero
+console.log(number)
+
+// 2 Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+const PI = 3.14 // numero
+console.log(PI)
+
+
+//2/C Dichiara una variabile chiamata radius e assegnale il valore 8.
+let radius = 8 // numero
+
+//Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle. 
+console.log(radius * 2 * PI)
+let circle = 50.24 // numero
+
+//Stampa il valore della variabile circle in console.
+console.log(circle)
+
+//3 Dichiara una variabile chiamata name e assegnale il valore “Carlo”.
+let name = 'Carlo'  // stringa
+console.log(name)
+
+//Successivamente, modifica il valore della variabile in “Marco”.
+name = 'Marco' // stringa
+console.log(name)
+
